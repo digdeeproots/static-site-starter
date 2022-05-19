ef1d789758f8d67cb620b78eb4551ea5d5b2596e
arlobelshee <arlo@belshee.com>
2022-05-19 16:21:27 -0400

    r. Remove commented-out code. That used to use gitlab with editorial workflow.

--- assets/cms/js/config/index.js
+++ assets/cms/js/config/index.js
@@ -1,25 +1,11 @@
-// Import the configuration of each collection from cms/config/collections
-// import blogPostsCollection from "./collections/blog-posts";
 import pagesCollection from "./collections/pages";
 import articlesCollection from "./collections/articles";
-// import pressReleasesCollection from "./collections/press-releases";
-// import servicesCollection from "./collections/services";
-// import siteConfigurationCollection from "./collections/site-configuration";
-// import testimonialsCollection from "./collections/testimonials";
 
-// Build the Netlify JS configuration object
 const config = {
   backend: {
     name: "git-gateway",
-    // name: "github",
-    // repo: "website",
     branch: "staging",
     cms_label_prefix: "content/editorial/",
-    // auth_type: "implicit",
-    // app_id: "MY_APP_ID",
-    // api_root: "https://my-self-hosted-gitlab.com/api/v4",
-    // base_url: "https://my-self-hosted-gitlab.com",
-    // auth_endpoint: "oauth/authorize",
   },
   load_config_file: false,
   publish_mode: "editorial_workflow",
@@ -28,12 +14,6 @@ const config = {
   collections: [
     pagesCollection,
 		articlesCollection,
-    // servicesCollection,
-    // blogPostsCollection,
-    // commonPageSectionsCollection,
-    // testimonialsCollection,
-    // pressReleasesCollection,
-    // siteConfigurationCollection,
   ],
 };
 
