65a4d146334d9332dd0bfe780af15217bd019da3
arlobelshee <arlo@belshee.com>
2022-05-19 12:18:37 -0400

    F. Remove the product page preview, since we don't have product pages.

--- assets/cms/js/cms.js
+++ assets/cms/js/cms.js
@@ -3,13 +3,11 @@ import CMS from "netlify-cms-app";
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
-import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 
