41f2db5728277dc55af5799a94189d387bf92f8c
arlobelshee <arlo@belshee.com>
2022-05-19 13:03:31 -0400

    t. Verify basic editorial workflow aspects of the CMS config.

--- assets/cms/js/config/config.test.js
+++ assets/cms/js/config/config.test.js
@@ -3,3 +3,17 @@ import config from ".";
 test('config exists', () => {
   expect(config).toBeDefined();
 });
+
+test('config defines editorial workflow', () => {
+  expect(config).toEqual(expect.objectContaining({
+		backend: {
+			name: 'git-gateway',
+			branch: expect.anything(),
+			cms_label_prefix: expect.stringMatching(/^content\//)
+		},
+		load_config_file: false,
+		publish_mode: 'editorial_workflow',
+		media_folder: 'static/img',
+		public_folder: '/img'
+	}));
+});
